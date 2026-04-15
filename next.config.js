9415d086ccda8a1fb82b605431c3e68f383666a8
ritwik-interactxp <ritwik@interactxp.in>
2026-04-15 23:55:26 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",   // Static export for Cloudflare Pages
+  trailingSlash: true,
+  images: {
+    unoptimized: true,  // Required for static export
+  },
+};
+
+module.exports = nextConfig;
