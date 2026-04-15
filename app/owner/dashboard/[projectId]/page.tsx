9cf58a781b19736112a3662401ff2fdd829842d2
ritwik-interactxp <ritwik@interactxp.in>
2026-04-16 01:19:37 +0530

    Delete app directory

--- app/owner/dashboard/[projectId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectDetailPage from "./client";
-
-export async function generateStaticParams() {
-  return [];
-}
-
-export default function Page(props: any) {
-  return <ProjectDetailPage {...props} />;
-}
