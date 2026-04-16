26e85ae4d3214354c47ea82278f4140e24d51a1d
ritwik-interactxp <ritwik@interactxp.in>
2026-04-16 12:38:12 +0530

    Delete app directory

--- app/portal/[projectId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import PortalPage from "./client";
-
-export async function generateStaticParams() {
-  return [];
-}
-
-export default function Page(props: any) {
-  return <PortalPage {...props} />;
-}
